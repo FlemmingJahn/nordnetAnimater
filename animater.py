b319ed34b73b102b480851391207da764eb6563a
Flemming <fjn@invisio.com>
2023-04-27 21:17:33 +0200

    Fixing calculation error

--- animater.py
+++ animater.py
@@ -13,10 +13,13 @@ with open(filename, 'r', encoding='utf-16') as csvfile:
 
 # show the plots
 class DepositsAndWithDrawals():
-    transaction_type = ['INDBETALING', 'HÆVNING']
+    transaction_inserts = ['INDBETALING','INDSÆTTELSE', 'Straksoverførsel']
+    transaction_type = ['INDBETALING', 'HÆVNING', "INDSÆTTELSE", 'Straksoverførsel']
     transaction_type_text = 'Transaktionstype'
+
     amount_text = 'Beløb'
     sum = [0]
+    sums = [0]
 
     # create figure with 2 subplots
     fig, (ax, ax2) = plt.subplots(1, 2, figsize=(12, 6))
@@ -24,69 +27,63 @@ class DepositsAndWithDrawals():
     text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
     title_text = fig.suptitle('My Title', fontsize=20)
     rects = 0
-    def analyze(self, row):
+    def analyze(self):
+        # calculate sums
+        for row in data:
+            if row['Transaktionstype'] in self.transaction_type:
+                self.sums.append(self.sums[-1] + float(row['Beløb'].replace(".", "").replace(",", ".")))
+            else:
+                self.sums.append(self.sums[-1])
+        self.ax2.set_title('Ind- og ud-betalinger')
+        self.ax.set_title('', fontsize=40)
+        self.ax.set_title('Ind- og ud-betalinger')
+
+    def plot_line(self, row, i):
         if row[self.transaction_type_text] in self.transaction_type:
             self.sum.append(self.sum[-1] + float(row[self.amount_text].replace(".", "").replace(",", ".")))
         else:
             self.sum.append(self.sum[-1])
 
-    def deposits_bars(self):
-        # calculate sums
-        sums = [0]
-        for row in data:
-            if row['Transaktionstype'] in ['INDBETALING', 'HÆVNING']:
-                sums.append(sums[-1] + float(row['Beløb'].replace(".", "").replace(",", ".")))
-            else:
-                sums.append(sums[-1])
-
         # first plot: animation of sum of INDBETALING transactions
-        self.ax.set_xlim(0, len(sums))
-        self.ax.set_ylim(0, max(sums))
-        self.ax.set_title('', fontsize=40)
+        self.ax.set_xlim(0, len(self.sums))
+        self.ax.set_ylim(0, max(self.sums))
 
-        # second plot: bar chart of total amount of INDBETALING and HÆVNING transactions
-        indbetaling_sum = 0
-        haevning_sum = 0
+        self.line.set_data(range(i + 1), self.sum[:i + 1])
+        self.text.set_text(f'Total sum: {self.sum[i]:,.0f} DKK')
+        self.title_text.set_text(f'{date_text}: {data[i][posting_date_text]}')
 
-        for row in data:
-            if row['Transaktionstype'] == 'INDBETALING':
-                indbetaling_sum += float(row['Beløb'].replace(".", "").replace(",", "."))
-            elif row['Transaktionstype'] == 'HÆVNING':
-                haevning_sum += float(row['Beløb'].replace(".", "").replace(",", "."))
-        totals = [indbetaling_sum * 1.2, haevning_sum]
+    def plot_bars(self, index):
+        for text_obj in self.ax2.texts:
+            text_obj.remove()
 
+        t = data[index]['Transaktionstype']
+        if t in self.transaction_inserts:
+            self.insert_sum += float(data[index]['Beløb'].replace(".", "").replace(",", "."))
+        elif data[index]['Transaktionstype'] == 'HÆVNING':
+            self.withdraw_sum += float(data[index]['Beløb'].replace(".", "").replace(",", "."))
+
+
+        totals = [self.insert_sum, self.withdraw_sum]
+        print (index, totals)
         self.rects = self.ax2.bar(['INDBETALING', 'HÆVNING'], totals, color=['green', 'red'])
 
-        self.ax2.set_title('Ind- og ud-betalinger')
-        self.ax.set_title('Ind- og ud-betalinger')
-        for i, total in enumerate(totals):
-            self.ax2.text(i, total + 0.1 * max(totals) / 2, f'{total:,.0f} DKK', ha='center')
+        for rect, total in zip(self.rects, totals):
+                rect.set_height(total)
+
+        for u, total in enumerate(totals):
+            self.ax2.text(u, total, f'{total:,.0f} DKK', ha='center')
+
+        return self.fig
+
+    def get_fig(self):
         return self.fig
 
     insert_sum = 0
     withdraw_sum = 0
 
     def update(self, i):
-        for text_obj in self.ax2.texts:
-            text_obj.remove()
-
-        self.line.set_data(range(i + 1), self.sum[:i + 1])
-        self.text.set_text(f'Total sum: {self.sum[i]:,.0f} DKK')
-        self.title_text.set_text(f'{date_text}: {data[i][posting_date_text]}')
-
-        if data[i]['Transaktionstype'] == 'INDBETALING':
-            self.insert_sum += float(data[i]['Beløb'].replace(".", "").replace(",", "."))
-        elif data[i]['Transaktionstype'] == 'HÆVNING':
-            self.withdraw_sum += float(data[i]['Beløb'].replace(".", "").replace(",", "."))
-
-        totals = [self.insert_sum, self.withdraw_sum]
-
-        for rect, total in zip(self.rects, totals):
-            rect.set_height(total)
-
-        for u, total in enumerate(totals):
-            self.ax2.text(u, total + 0.1 * max(totals) / 2, f'{total:,.0f} DKK', ha='center')
-
+        self.plot_line(data[i], i)
+        self.plot_bars(i)
         return self.line, self.text, self.title_text
 
     def figure(self):
@@ -94,22 +91,28 @@ class DepositsAndWithDrawals():
 
 class Animate():
     deposits_and_withdrawals = DepositsAndWithDrawals()
-    deposits_and_withdrawals2 = DepositsAndWithDrawals()
 
-    fig = deposits_and_withdrawals.deposits_bars()
+    fig = deposits_and_withdrawals.get_fig()
 
     def update_data(self):
+        is_first_frame = True  # flag to indicate the first frame
+
         def update(i):
+            nonlocal is_first_frame
+            if is_first_frame:
+                is_first_frame = False
+                return None  # skip the first frame
+            print (i)
             #    line.set_data(range(i+1), sums[:i+1])
             # Clear the text objects in ax2
 
-            self.deposits_and_withdrawals.analyze(data[i])
+            #self.deposits_and_withdrawals.analyze(data[i])
             return self.deposits_and_withdrawals.update(i)
         return update
 
     def plot(self):
-        my_update_data = self.update_data()
-        ani1 = animation.FuncAnimation(self.fig, my_update_data, frames=len(data), interval=0, repeat=False)
+        self.deposits_and_withdrawals.analyze()
+        ani1 = animation.FuncAnimation(self.fig,  self.update_data(), frames=len(data), interval=0, repeat=False)
         plt.show()
 
 
